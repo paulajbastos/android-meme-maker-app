4ade157e41581fd1f2109557ec6fe4283b65fff0
paulajbastos <paulajbastos@gmail.com>
2015-03-24 17:33:47 -0300

    first commit

---
+++ app/src/main/java/com/paulajbastos/mememaker/MemeMakerApplicationSettings.java
@@ -0,0 +1,17 @@
+package com.paulajbastos.mememaker;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.paulajbastos.mememaker.utils.StorageType;
+
+/**
+ * Created by Evan Anger on 8/13/14.
+ */
+public class MemeMakerApplicationSettings {
+
+    public MemeMakerApplicationSettings(Context context) {
+
+    }
+}
---
+++ app/src/main/java/com/paulajbastos/mememaker/database/MemeDatasource.java
@@ -0,0 +1,26 @@
+package com.paulajbastos.mememaker.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
+import com.paulajbastos.mememaker.models.Meme;
+import com.paulajbastos.mememaker.models.MemeAnnotation;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by Evan Anger on 8/17/14.
+ */
+public class MemeDatasource {
+
+    private Context mContext;
+    private MemeSQLiteHelper mMemeSqlLiteHelper;
+
+    public MemeDatasource(Context context) {
+        mContext = context;
+    }
+}
