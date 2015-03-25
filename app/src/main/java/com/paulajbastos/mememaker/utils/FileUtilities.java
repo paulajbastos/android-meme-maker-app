768f6e7422b99718cc02e87c9bf45322a8128073
paulajbastos <paulajbastos@gmail.com>
2015-03-24 22:36:37 -0300

    Refactoring for Internal Storage

--- app/src/main/java/com/paulajbastos/mememaker/utils/FileUtilities.java
+++ app/src/main/java/com/paulajbastos/mememaker/utils/FileUtilities.java
@@ -24,7 +24,45 @@ import java.io.OutputStream;
 public class FileUtilities {
 
     public static void saveAssetImage(Context context, String assetName) {
+        File fileDirectory = context.getFilesDir();
+        File fileToWrite = new File(fileDirectory, assetName);
 
+        AssetManager assetManager =  context.getAssets();
+        try {
+            InputStream in = assetManager.open(assetName);
+            FileOutputStream out = new FileOutputStream(fileToWrite);
+            copyFiles(in,out);
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void copyFiles(InputStream in, OutputStream out) throws IOException{
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer,0,read);
+        }
+
+    }
+
+    public  static File [] listFiles(Context context){
+        final File fileDirectory = context.getFilesDir();
+        File [] filteredFiles = fileDirectory.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                if(file.getAbsolutePath().contains(".jpg")){
+                    return true;
+                }else{
+                    return false;
+                }
+
+            }
+        });
+        return filteredFiles;
     }
 
     public static Uri saveImageForSharing(Context context, Bitmap bitmap,  String assetName) {
